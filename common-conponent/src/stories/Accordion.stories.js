35cf101b7e2b25733b96d0aa4ce042872d29f6e5
hmhmhm0523 <mhuang6@ncsu.edu>
2021-02-24 15:42:45 -0500

    Test the Github Actions

--- common-conponent/src/stories/Accordion.stories.js
+++ common-conponent/src/stories/Accordion.stories.js
@@ -17,20 +17,3 @@ Normal.args = {
   // primary: true,
   // label: 'Button',
 };
-
-// export const Secondary = Template.bind({});
-// Secondary.args = {
-//   label: 'Button',
-// };
-
-// export const Large = Template.bind({});
-// Large.args = {
-//   size: 'large',
-//   label: 'Button',
-// };
-
-// export const Small = Template.bind({});
-// Small.args = {
-//   size: 'small',
-//   label: 'Button',
-// };
