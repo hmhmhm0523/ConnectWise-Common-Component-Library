bbc968e48ca29cb5c599a17ddff4161d1e943813
hmhmhm0523 <mhuang6@ncsu.edu>
2021-02-24 15:35:17 -0500

    More decoration of the Accordion

--- common-conponent/src/stories/Accordion.js
+++ common-conponent/src/stories/Accordion.js
@@ -6,7 +6,13 @@ import './accordion.scss';
  * Primary UI component for user interaction
  */
 export const Accordion = ({ collapsed, statusColor, title, children, ...props }) => {
+
   const mode = collapsed ? 'accordion--collapsed' : 'accordion--expanded';
+  function handleClick(e) {
+    e.preventDefault();
+    
+    e.target.parentNode.parentNode.classList.toggle("accordion--collapsed");
+  }
   return (
     <div
       className= {'accordion ' + mode}
@@ -14,13 +20,17 @@ export const Accordion = ({ collapsed, statusColor, title, children, ...props })
       {...props}
     >
       <div className='accordion-header' style={{borderLeftColor: statusColor}}>
+        <input type='checkbox'></input>
         <h3>{title}</h3>
-        <a>v</a>
+        <span className='spacer'></span>
+        <a onClick={handleClick}></a>
       </div>
       <div className='accordion-content'>{children}</div>
     </div>
   );
 };
+
+
 
 Accordion.propTypes = {
   /**
@@ -47,8 +57,8 @@ Accordion.propTypes = {
 
 Accordion.defaultProps = {
   collapsed: false,
-  statusColor: '#cc3232',
-  title: 'Accordion Title',
-  children: 'Accordion Content',
+  statusColor: '#E6E6E6',
+  title: 'Accordion Row Title',
+  children: 'Detail Panel',
   onClick: undefined,
 };
--- common-conponent/src/stories/Accordion.stories.js
+++ common-conponent/src/stories/Accordion.stories.js
@@ -12,25 +12,25 @@ export default {
 
 const Template = (args) => <Accordion {...args} />;
 
-export const Primary = Template.bind({});
-Primary.args = {
-  primary: true,
-  label: 'Button',
+export const Normal = Template.bind({});
+Normal.args = {
+  // primary: true,
+  // label: 'Button',
 };
 
-export const Secondary = Template.bind({});
-Secondary.args = {
-  label: 'Button',
-};
+// export const Secondary = Template.bind({});
+// Secondary.args = {
+//   label: 'Button',
+// };
 
-export const Large = Template.bind({});
-Large.args = {
-  size: 'large',
-  label: 'Button',
-};
+// export const Large = Template.bind({});
+// Large.args = {
+//   size: 'large',
+//   label: 'Button',
+// };
 
-export const Small = Template.bind({});
-Small.args = {
-  size: 'small',
-  label: 'Button',
-};
+// export const Small = Template.bind({});
+// Small.args = {
+//   size: 'small',
+//   label: 'Button',
+// };
